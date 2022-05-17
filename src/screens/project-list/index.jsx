09c34e820596ee27e699a5c98b1fce4420f68e3f
Morphone0429 <179464330@qq.com>
2022-05-17 21:47:37 +0800

    3-3 over

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { SearchPanel } from './search-panel';
 import { List } from './list';
-import { cleanObject } from 'utils';
+import { cleanObject, useDebounce } from 'utils';
 import * as qs from 'qs';
+import { useMount } from '../../utils';
 
 const apiUrl = process.env.REACT_APP_API_URL; // 通过打包命令会读取不同的接口变量 .env
 
@@ -11,24 +12,25 @@ export const ProjectListScreen = () => {
     name: '',
     personId: '',
   });
+  const debounceParam = useDebounce(param, 2000);
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
   useEffect(() => {
-    fetch(`${apiUrl}/projects?name=${param.name}&personId=${param.personId}`);
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async res => {
+    // fetch(`${apiUrl}/projects?name=${param.name}&personId=${param.personId}`);
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async res => {
       if (res.ok) {
         setList(await res.json());
       }
     });
-  }, [param]);
+  }, [debounceParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async res => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
+
 // 判断值是否为0
 export const isFalsy = value => (value === 0 ? false : !value);
 
+// 清除对象中value为空的key-value
 export const cleanObject = object => {
   // Object.assign({},object) 1
   const result = { ...object };
@@ -12,4 +15,24 @@ export const cleanObject = object => {
   });
 
   return result;
+};
+
+// 自定义hook  以use开头
+export const useMount = callback => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // 每次在value变化以后 设置一个定时器
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    // 清理定时器
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
 };
