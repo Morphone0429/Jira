491b6732b9cd42ad1923874dd39c591109730d77
Morphone0429 <179464330@qq.com>
2022-05-17 20:06:55 +0800

    3-1

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,10 @@
+import SearchPanel from './search-panel';
+import List from './List';
+export const ProjectListScreen = () => {
+  return (
+    <div>
+      <SearchPanel />
+      <List />
+    </div>
+  );
+};
