7396eef1dd9434b2c2a6d6a0fa6c90a88cf1995d
Morphone0429 <179464330@qq.com>
2022-05-17 21:03:56 +0800

    m

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@
 export const isFalsy = value => (value === 0 ? false : !value);
 
 export const cleanObject = object => {
-  // Object.assign({},object)
+  // Object.assign({},object) 1
   const result = { ...object };
   Object.keys(result).forEach(key => {
     const value = object[key];
