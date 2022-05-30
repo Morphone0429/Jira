b49e39061b1d741a96a2886811bbde1ef0494a10
chaoqiang.lou <chaoqiang.l@renrenbit.com>
2022-05-30 13:54:49 +0800

    6-4

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,70 @@
+import styled from '@emotion/styled';
 import { useAuth } from 'context/auth-context';
 import { ProjectListScreen } from 'screens/project-list';
+
+// flex grid各自的应用场景
+// 1.一维布局还是二维布局   一维->flex  二维->grid
+// 2.从内容出发还是布局出发
+// 内容：你先有一组内容(数组一般不固定)，然后希望他们均匀的分布在容器中  flex
+// 布局  先规划网格  数量固定   grid
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>logout</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>Logo</h3>
+          <h3>Logo</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>logout</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+export const Row = styled.div<{
+  gap?: number | boolean;
+  between?: boolean;
+  marginBottom?: number;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => (props.between ? 'space-between' : undefined)};
+  margin-bottom: ${(props) => props.marginBottom + 'rem'};
+  > * {
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    margin-right: ${(props) =>
+      typeof props.gap === 'number'
+        ? props.gap + 'rem'
+        : props.gap
+        ? '2rem'
+        : undefined};
+  }
+`;
+
+// grid-area 用来给grid子元素起名字
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  /* display: flex; */
+  /* overflow: hidden; */
+`;
