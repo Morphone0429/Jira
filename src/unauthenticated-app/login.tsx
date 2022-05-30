ef4ead7783cfc5eeb031125070c5390fd870458e
chaoqiang.lou <chaoqiang.l@renrenbit.com>
2022-05-30 12:48:32 +0800

    6-3

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from 'context/auth-context';
 import { Form, Input, Button } from 'antd';
+import { LongButton } from 'unauthenticated-app';
 export const apiUrl = process.env.REACT_APP_API_URL;
 export const LoginScreen = () => {
   const { login, user } = useAuth();
@@ -9,16 +10,22 @@ export const LoginScreen = () => {
 
   return (
     <Form onFinish={handleSubmit}>
-      <Form.Item name={'username'} rules={[{ required: true, message: '请输入用户名' }]}>
-        <Input placeholder='用户名' type='text' id={'username'} />
+      <Form.Item
+        name={'username'}
+        rules={[{ required: true, message: '请输入用户名' }]}
+      >
+        <Input placeholder="用户名" type="text" id={'username'} />
       </Form.Item>
-      <Form.Item name={'password'} rules={[{ required: true, message: '请输入密码' }]}>
-        <Input placeholder='密码' type='password' id={'username'} />
+      <Form.Item
+        name={'password'}
+        rules={[{ required: true, message: '请输入密码' }]}
+      >
+        <Input placeholder="密码" type="password" id={'username'} />
       </Form.Item>
       <Form.Item>
-        <Button type='primary' htmlType={'submit'}>
+        <LongButton type="primary" htmlType={'submit'}>
           登录
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from 'context/auth-context';
 import { FormEvent } from 'react';
 import { Form, Input, Button } from 'antd';
+import { LongButton } from 'unauthenticated-app';
 export const apiUrl = process.env.REACT_APP_API_URL;
 export const RegisterScreen = () => {
   const { register, user } = useAuth();
@@ -11,16 +12,22 @@ export const RegisterScreen = () => {
 
   return (
     <Form onFinish={handleSubmit}>
-      <Form.Item name={'username'} rules={[{ required: true, message: '请输入用户名' }]}>
-        <Input placeholder='用户名' type='text' id={'username'} />
+      <Form.Item
+        name={'username'}
+        rules={[{ required: true, message: '请输入用户名' }]}
+      >
+        <Input placeholder="用户名" type="text" id={'username'} />
       </Form.Item>
-      <Form.Item name={'password'} rules={[{ required: true, message: '请输入密码' }]}>
-        <Input placeholder='密码' type='password' id={'username'} />
+      <Form.Item
+        name={'password'}
+        rules={[{ required: true, message: '请输入密码' }]}
+      >
+        <Input placeholder="密码" type="password" id={'username'} />
       </Form.Item>
       <Form.Item>
-        <Button type='primary' htmlType={'submit'}>
+        <LongButton type="primary" htmlType={'submit'}>
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
