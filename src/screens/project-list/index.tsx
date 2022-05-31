8e36f626d0c40aaa0130084385061f636feca07f
Morphone0429 <179464330@qq.com>
2022-05-31 08:23:43 +0800

    7-1

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from 'react';
 import { SearchPanel } from './search-panel';
 import { List } from './list';
 import { cleanObject, useDebounce } from 'utils';
-import * as qs from 'qs';
 import { useMount } from '../../utils';
 import { useHttp } from 'utils/http';
 import styled from '@emotion/styled';
+import { Typography } from 'antd';
 
 const apiUrl = process.env.REACT_APP_API_URL; // 通过打包命令会读取不同的接口变量 .env
 
@@ -18,7 +18,7 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<null | Error>(null);
+  const [error, setError] = useState<null | Error>(null); //异常处理
   const client = useHttp();
 
   useEffect(() => {
@@ -27,6 +27,10 @@ export const ProjectListScreen = () => {
       data: cleanObject(debounceParam),
     })
       .then(setList)
+      .catch(error => {
+        setError(error);
+        setList([]);
+      })
       .finally(() => setLoading(false));
   }, [debounceParam]);
 
@@ -36,7 +40,9 @@ export const ProjectListScreen = () => {
 
   return (
     <Container>
+      <h1>项目列表</h1>
       <SearchPanel param={param} setParam={setParam} users={users} />
+      {error ? <Typography.Text type='danger'>{error.message}</Typography.Text> : null}
       <List dataSource={list} users={users} loading={loading} />
     </Container>
   );
