b7cc4eadc256fdf6bfdb3de028be8bfb705df13b
chaoqiang.lou <chaoqiang.l@renrenbit.com>
2022-06-01 12:39:41 +0800

    7-5

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { ProjectListScreen } from './screens/project-list';
 import './App.css';
 import React from 'react';
 
 import { useAuth } from 'context/auth-context';
 import { AuthenticatedApp } from 'authenticated-app';
 import { UnauthenticatedApp } from 'unauthenticated-app';
+import { ErrorBoundary } from 'components/error-boundary';
+import { FullPageErrorFallback } from 'components/lib';
 function App() {
   const { user } = useAuth();
-  return <div className='App'>{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>;
+  return (
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
+    </div>
+  );
 }
 export default App;
