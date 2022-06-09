44aa6f37c66cf62fd1ffee4a271353990a33f07d
chaoqiang.lou <chaoqiang.l@renrenbit.com>
2022-06-09 12:57:29 +0800

    10-12

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,16 +1,19 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useCallback } from 'react';
 import * as auth from 'auth-provider';
 import { User } from 'screens/project-list/search-panel';
 import { http } from 'utils/http';
 import { useMount } from 'utils';
 import { useAsync } from 'utils/use-async';
 import { FullPageErrorFallback, FullPageLoading } from 'components/lib';
-interface AuthForm {
+import { useDispatch, useSelector } from 'react-redux';
+import { bootstrap, selectUser } from 'store/auth.slice';
+import * as authStore from 'store/auth.slice';
+export interface AuthForm {
   username: string;
   password: string;
 }
 
-const bootstapUser = async () => {
+export const bootstapUser = async () => {
   let user = null;
   const token = auth.getToken(); //有token时
   if (token) {
@@ -20,38 +23,13 @@ const bootstapUser = async () => {
   return user;
 };
 
-const AuthContext = React.createContext<
-  | {
-      user: User | null;
-      register: (form: AuthForm) => Promise<void>;
-      login: (form: AuthForm) => Promise<void>;
-      logout: () => Promise<void>;
-    }
-  | undefined
->(undefined);
-AuthContext.displayName = 'AuthContext';
-
-const AuthProVider = ({ children }: { children: ReactNode }) => {
-  // const [user, setUser] = useState<User | null>(null);
-  const {
-    data: user,
-    error,
-    isLoading,
-    isIdle,
-    isError,
-    run,
-    setData: setUser,
-  } = useAsync<User | null>();
-
-  const login = (form: AuthForm) => auth.login(form).then(setUser); // point free  消参  setUser ===  user => setUser(user)
-  const register = (form: AuthForm) =>
-    auth.register(form).then((user) => setUser(user));
-  const logout = () => auth.logout().then(() => setUser(null));
+export const AuthProVider = ({ children }: { children: ReactNode }) => {
+  const { error, isLoading, isIdle, isError, run } = useAsync<User | null>();
+  // @ts-ignore
+  const dispatch: (...args: unknown[]) => Promise<User> = useDispatch();
 
   useMount(() => {
-    // 当页面加载是 ,调用bootstapUser
-    // bootstapUser().then(setUser);
-    run(bootstapUser());
+    run(dispatch(bootstrap()));
   });
   // 加载或初始时 返回
   if (isLoading || isIdle) {
@@ -59,24 +37,25 @@ const AuthProVider = ({ children }: { children: ReactNode }) => {
   }
   // 返回失败
   if (isError) {
-    return (
-      <FullPageErrorFallback error={error as Error}></FullPageErrorFallback>
-    );
+    return <FullPageErrorFallback error={error as Error}></FullPageErrorFallback>;
   }
-  return (
-    <AuthContext.Provider
-      children={children}
-      value={{ user, login, register, logout }}
-    ></AuthContext.Provider>
+  return <div>{children}</div>;
+};
+
+export const useAuth = () => {
+  // @ts-ignore
+  const dispatch: (...args: unknown[]) => Promise<User> = useDispatch();
+  const user = useSelector(selectUser);
+  const login = useCallback((form: AuthForm) => dispatch(authStore.login(form)), [dispatch]);
+  const register = useCallback(
+    (form: AuthForm) => dispatch(authStore.register(form)),
+    [dispatch]
   );
+  const logout = useCallback(() => dispatch(authStore.logout()), [dispatch]);
+  return {
+    user,
+    login,
+    register,
+    logout,
+  };
 };
-
-const useAuth = () => {
-  const context = React.useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth必须在authProvider中使用');
-  }
-  return context;
-};
-
-export { AuthProVider, useAuth };
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -2,19 +2,13 @@ import { useAuth } from 'context/auth-context';
 import { Form, Input, Button } from 'antd';
 import { LongButton } from 'unauthenticated-app';
 import { useAsync } from 'utils/use-async';
+import { useDispatch } from 'react-redux';
 export const apiUrl = process.env.REACT_APP_API_URL;
-export const LoginScreen = ({
-  onError,
-}: {
-  onError: (error: Error) => void;
-}) => {
+export const LoginScreen = ({ onError }: { onError: (error: Error) => void }) => {
   const { login, user } = useAuth();
   const { run, isLoading } = useAsync(undefined, { throwOnError: true }); // 不使用这里的error原因是  setError 是异步操作 不能实时获取最新值  不要和同步代码混一起
-
-  const handleSubmit = async (values: {
-    username: string;
-    password: string;
-  }) => {
+  // const dispatch = useDispatch();
+  const handleSubmit = async (values: { username: string; password: string }) => {
     try {
       await run(login(values));
       // await login(values);
@@ -25,16 +19,10 @@ export const LoginScreen = ({
 
   return (
     <Form onFinish={handleSubmit}>
-      <Form.Item
-        name={'username'}
-        rules={[{ required: true, message: '请输入用户名' }]}
-      >
+      <Form.Item name={'username'} rules={[{ required: true, message: '请输入用户名' }]}>
         <Input placeholder="用户名" type="text" id={'username'} />
       </Form.Item>
-      <Form.Item
-        name={'password'}
-        rules={[{ required: true, message: '请输入密码' }]}
-      >
+      <Form.Item name={'password'} rules={[{ required: true, message: '请输入密码' }]}>
         <Input placeholder="密码" type="password" id={'username'} />
       </Form.Item>
       <Form.Item>
