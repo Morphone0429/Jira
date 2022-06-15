7b6d0916ab46dc7d19ec9156f80faa2035fe7386
chaoqiang.lou <chaoqiang.l@renrenbit.com>
2022-06-15 14:17:33 +0800

    12-2

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,10 +1,6 @@
-import { useCallback, useEffect } from 'react';
-import { QueryKey, useMutation, useQuery, useQueryClient } from 'react-query';
+import { QueryKey, useMutation, useQuery } from 'react-query';
 import { Project } from 'screens/project-list/list';
-import { useProjectsSearchParams } from 'screens/project-list/util';
-import { cleanObject } from 'utils';
 import { useHttp } from './http';
-import { useAsync } from './use-async';
 import { useAddConfig, useDeleteConfig, useEditConfig } from './use-optimistic-options';
 
 export const useProjects = (param?: Partial<Project>) => {
@@ -13,46 +9,8 @@ export const useProjects = (param?: Partial<Project>) => {
   // return useQuery<Project[], Error>(['projects', param], () =>
   //   client('projects', { data: cleanObject(param || {}) })
   // );
-
   return useQuery<Project[]>(['projects', param], () => client('projects', { data: param }));
 };
-
-// export const useEditProject = () => {
-//   const client = useHttp();
-//   const queryClient = useQueryClient();
-//   const [searchParams] = useProjectsSearchParams();
-//   const queryKey = ['projects', searchParams];
-//   return useMutation(
-//     (params: Partial<Project>) =>
-//       client(`projects/${params.id}`, {
-//         data: params,
-//         method: 'PATCH',
-//       }),
-//     {
-//       // onSuccess: () => queryClient.invalidateQueries('projects'), // 'projects' 和 useProjects key 保持一致  使用了缓存  并及时刷新
-//       onSuccess: () => queryClient.invalidateQueries(queryKey), // 'projects' 和 useProjects key 保持一致  使用了缓存  并及时刷新
-//       async onMutate(target) {
-//         // 在请求回来之前就进行更新  onMutate只要useMutation一调用 就会执行
-//         const previousItems = queryClient.getQueryData(queryKey);
-//         queryClient.setQueryData(queryKey, (old?: Project[]) => {
-//           return (
-//             old?.map((project) =>
-//               project.id === target.id ? { ...project, ...target } : project
-//             ) || []
-//           );
-//         });
-//         return { previousItems };
-//       },
-//       onError(error, newItem, context) {
-//         // 请求出错时  状态回滚
-//         queryClient.setQueryData(
-//           queryKey,
-//           (context as { previousItems: Project[] }).previousItems
-//         );
-//       },
-//     }
-//   );
-// };
 
 export const useEditProject = (queryKey: QueryKey) => {
   const client = useHttp();
